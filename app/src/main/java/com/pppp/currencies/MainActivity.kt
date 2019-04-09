880aa2f9514b96b2906da772f6f00d2b28769b0c
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2019-04-09 17:23:33 +0100

    Experiment OK  #Pleasi enter the commit message for your changes. Lines starting

--- app/src/main/java/com/pppp/currencies/MainActivity.kt
+++ app/src/main/java/com/pppp/currencies/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.pppp.currencies
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.pppp.currencies.network.client.RetrofitClient
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,13 +23,31 @@ class MainActivity : AppCompatActivity() {
         val client = RetrofitClient(cacheDir = cacheDir)
         GlobalScope.launch(Dispatchers.IO) {
             try {
-                val result = client.getRates("EUR")
+                //val result = client.getRates("EUR")
                 foo()
             } catch (exception: Exception) {
                 foo()
             }
-
         }
+
+        val time = Flowable.interval(1, TimeUnit.SECONDS)
+        val delay = Observable.just(1).delay(3, TimeUnit.SECONDS)
+        time
+            .onBackpressureDrop()
+            .doOnNext { Log.e("foo", "producing " + it) }
+            .subscribeOn(Schedulers.io())
+            .flatMap({
+                Thread.sleep(3 * 1000)
+                Flowable.just(it)
+            }, 1)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { Log.e("foo", "reciveed " + it) }
+    }
+
+    private fun rand(): Long {
+        val nextLong = Random.nextLong(5 * 1000)
+        Log.e("foo", "sleeping " + nextLong)
+        return nextLong
     }
 
     private fun foo() {
