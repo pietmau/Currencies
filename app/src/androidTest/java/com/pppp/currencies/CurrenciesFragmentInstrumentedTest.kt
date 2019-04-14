cd5678cdfdfae4472d0c00cb565c2f488d351a4e
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2019-04-14 13:58:41 +0100

    Add Espresso tests

--- app/src/androidTest/java/com/pppp/currencies/CurrenciesFragmentInstrumentedTest.kt
+++ app/src/androidTest/java/com/pppp/currencies/CurrenciesFragmentInstrumentedTest.kt
@@ -1,19 +1,33 @@
 package com.pppp.currencies
 
 import androidx.lifecycle.MutableLiveData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.pppp.currencies.app.CurrencyApp
 import com.pppp.currencies.data.pokos.Currency
 import com.pppp.currencies.presentation.view.CurrenciesActivity
+import com.pppp.currencies.presentation.view.custom.CurrenciesViewHolder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.math.BigDecimal
+
+private const val ITL = "ITL"
+private const val GBP = "GBP"
+private const val COUNTRY = "some_country"
+private const val UK = "uk"
 
 @RunWith(AndroidJUnit4::class)
 class CurrenciesFragmentInstrumentedTest {
     private val data: MutableLiveData<List<Currency>> = MutableLiveData()
+    private val itl = Currency(ITL, "", BigDecimal(1), COUNTRY, "www")
+    private val gbp = Currency(GBP, "", BigDecimal(2), UK, "www")
 
     @get:Rule
     var activityRule: ActivityTestRule<CurrenciesActivity> = object :
@@ -22,17 +36,45 @@ class CurrenciesFragmentInstrumentedTest {
             val instrumentation = InstrumentationRegistry.getInstrumentation()
             val app = instrumentation.targetContext.applicationContext as CurrencyApp
             val viewmodel = TestCurrenciesViewModel(data)
-            val appComponent =
-                DaggerTestAppComponent.builder().testAppModule(TestAppModule(viewmodel)).build()
+            val builder = DaggerTestAppComponent.builder()
+            val appComponent = builder.testAppModule(TestAppModule(viewmodel)).build()
             app.component = appComponent
         }
     }
 
     @Test
-    fun useAppContext() {
-        data.postValue(emptyList())
-        // Context of the app under test.
-        //    val appContext = InstrumentationRegistry.getTargetContext()
-        //  assertEquals("com.pppp.currencies", appContext.packageName)
+    fun when_receives_then_shows() {
+        //WHEN
+        data.postValue(listOf(itl))
+        // THEN
+        checkItemAtPosition()
+    }
+
+
+    @Test
+    fun when_click_then_movestoTop() {
+        //WHEN
+        data.postValue(listOf(itl, gbp))
+        // THEN
+        checkItemAtPosition()
+        checkItemAtPosition(1, GBP, UK, "2.0000")
+        onView(withId(R.id.recycler))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<CurrenciesViewHolder>(1, click()))
+        checkItemAtPosition(0, GBP, UK, "2.0000")
+        checkItemAtPosition(1)
+    }
+
+    private fun checkItemAtPosition(
+        position: Int = 0,
+        symbol: String = ITL,
+        country: String = COUNTRY,
+        amount: String = "1.0000"
+    ) {
+        onView(withId(R.id.recycler))
+            .check(matches(atPosition(position, hasDescendant(withText(symbol)))))
+        onView(withId(R.id.recycler))
+            .check(matches(atPosition(position, hasDescendant(withText(country)))))
+        onView(withId(R.id.recycler))
+            .check(matches(atPosition(position, hasDescendant(withText(amount)))))
     }
 }
--- app/src/main/java/com/pppp/currencies/presentation/imageloader/PicassoImageLoader.kt
+++ app/src/main/java/com/pppp/currencies/presentation/imageloader/PicassoImageLoader.kt
@@ -1,12 +1,15 @@
 package com.pppp.currencies.presentation.imageloader
 
 import android.widget.ImageView
+import com.pppp.currencies.R
 import com.squareup.picasso.Picasso
 
 class PicassoImageLoader : ImageLoader {
     override fun loadImage(url: String, view: ImageView) {
         Picasso.get()
             .load(url)
+            .error(R.drawable.revoult)
+            .placeholder(R.drawable.revoult)
             .into(view);
     }
 }
